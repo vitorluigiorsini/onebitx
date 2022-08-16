8e8c52a22a735af58836a8788ff8c7afc23095f6
Vitor Orsini <vitorl85@gmail.com>
2022-08-16 12:05:05 -0300

    Adiciona projeto onebitX

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const navBar = document.querySelector(".navbarMenu");
+
+let scrollPosition1 = scrollY;
+
+document.addEventListener("scroll", (event) => {
+  const scrollPosition2 = scrollY;
+  if (scrollPosition2 > scrollPosition1) {
+    navBar.style.transform = "translateY(-105px)";
+    scrollPosition1 = scrollPosition2;
+  } else {
+    navBar.style.transform = "translateY(0)";
+    scrollPosition1 = scrollPosition2;
+  }
+
+  if (scrollY > 600) {
+    navBar.classList.add("bgColor");
+  } else {
+    navBar.classList.remove("bgColor");
+  }
+});
+
+const offcanvasBackground = document.getElementById("offcanvasWithBackdrop");
+
+window.addEventListener("resize", (event) => {
+  if (window.innerWidth < 992) {
+    offcanvasBackground.classList.add("bg-dark");
+  } else {
+    offcanvasBackground.classList.remove("bg-dark");
+  }
+});
+
+window.onload = () => {
+  if (window.innerWidth < 992) {
+    offcanvasBackground.classList.add("bg-dark");
+  } else {
+    offcanvasBackground.classList.remove("bg-dark");
+  }
+};
